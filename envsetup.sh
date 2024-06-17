8e448fc8bfd298026925deba5549267f732670f5
Romuald Jeanne <romuald.jeanne@st.com>
2024-06-17 09:14:32 +0200

    Add support of Ubuntu 24.04
    Change-Id: I758c12ef483b9c0b5c240adbb14b836ffa1215c0
    Signed-off-by: Romuald Jeanne <romuald.jeanne@st.com>
    Reviewed-on: https://gerrit.st.com/c/oeivi/oe/st/stoe-script/+/387855

--- envsetup.sh
+++ envsetup.sh
@@ -10,7 +10,7 @@ _SITECONFSAMPLE_PATH=$(dirname $(readlink -f ${BASH_SOURCE}))
 # Set supported Linux Distrib Release
 #
 _SUPPORTED_LINUX_DISTRIB="Ubuntu"
-_SUPPORTED_UBUNTU_RELEASE="20.04 22.04"
+_SUPPORTED_UBUNTU_RELEASE="20.04 22.04 24.04"
 
 #----------------------------------------------
 # Set default layer root
